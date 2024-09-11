d7e8a15f0aaf3c7bb6d5432264cc3a2273e21521
WithersE22 <ST10090150@vcconnect.edu.za>
2024-09-11 10:23:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using SitemastersIT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SitemastersIT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Initialize Firebase Admin SDK using the JSON file
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("/Users/Ethan/Projects/SitemastersIT/SitemastersIT/config/sitemastersit-firebase-adminsdk-hogqb-884f1fd39d.json")
+            });
+
+            // Add ApplicationDbContext to the DI container and configure the connection string
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
